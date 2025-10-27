93a89c36fa224b63747e0a20069f4eb685433d49
AdrianoFossati <adriano.fossati@netplus.ch>
2025-10-27 12:27:27 +1000

    Minor changes to the testing phase of the tutorial to make it clearer and minor changes to some functions to run with Sentinel images.

--- counting_wh/wh_utils/testing.py
+++ counting_wh/wh_utils/testing.py
@@ -64,7 +64,7 @@ def parse_config(config: str) -> dict:
 
 def prepare(run_folder, config):
     """
-    Given a folder, find all the tif files are create a png for each one.
+    Given a folder, find all the .tif files from Planet and create a png for each one.
     Also rename tif files if required.
 
     Args:
@@ -127,6 +127,71 @@ def prepare(run_folder, config):
                         stride=config["img_stride"],
                     )
 
+
+def prepare_S2(run_folder, config):
+    """
+    Given a folder, find all the .tif files from Sentinel and create a png for each one.
+    Also rename tif files if required.
+
+    Args:
+
+        run_folder (str): The folder to prepare
+        config (dict): The configuration dictionary
+
+    Returns:
+
+        None
+    """
+    config = parse_config(config)
+    img_folder = config["raw_images"]  # folder with the tif files
+    save_folder = os.path.join(config["path"], config["pngs"])
+    os.makedirs(save_folder, exist_ok=True)
+    print('read the file path correctly')
+    print(save_folder)
+    print(img_folder)
+    for root, _, files in os.walk(img_folder):
+        for file in files:
+            if file == "composite.tif":
+                # find the json file:
+                date_file = [f for f in files if f.endswith("xml")][0]
+                if not date_file: 
+                    print(f"Warning: No XML file found in {root}, skipping renaming.")
+                    continue
+
+                date = date_file.split("_")[0]
+                aoi = os.path.basename(root).split("_")[-2].split("/")[-1]
+                print(root, aoi)
+                name = f"{date}_{aoi}.tif"
+                print(name)
+                print(f"Renaming {file} to {name}")
+
+                os.rename(os.path.join(root, file), os.path.join(root, name))
+
+                # want to create a png for this
+                new_name = os.path.join(save_folder, f"{name.split('.')[0]}.png")
+                if not os.path.exists(new_name):
+                    print(f"Creating PNG for {name}")
+                    ics.create_padded_png_S2(
+                        root,
+                        save_folder,
+                        name,
+                        tile_size=config["img_size"],
+                        stride=config["img_stride"],
+                    )
+                    
+            # if the file is a tif and first part is a date, don't need to rename
+            elif file.endswith("tif") and file.split("_")[0].isdigit():
+                # check if we have already created a png for this
+                new_name = os.path.join(save_folder, f"{file.split('.')[0]}.png")
+                if not os.path.exists(new_name):
+                    print(f"Creating png for {file}")
+                    ics.create_padded_png_S2(
+                        root,
+                        save_folder,
+                        file,
+                        tile_size=config["img_size"],
+                        stride=config["img_stride"],
+                    )
 
 
 
